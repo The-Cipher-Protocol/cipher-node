8d89945d8348578f5b863843d325233d29967023
Anonymous <anonymous@example.com>
2024-12-23 04:05:02 +0600

    init

---
+++ versioning/versioning.go
@@ -0,0 +1,14 @@
+package versioning
+
+var (
+	// Version is the main version at the moment.
+	// Commit is the git commit that the binary was built on
+	// BuildTime is the timestamp of the build
+	// Embedded by --ldflags on build time
+	// Versioning should follow the SemVer guidelines
+	// https://semver.org/
+	Version   = "1.0.0" // Updated to the next patch version
+	Branch    = "beta"
+	BuildTime = "10/30/2024"
+	//Commit    string
+)
